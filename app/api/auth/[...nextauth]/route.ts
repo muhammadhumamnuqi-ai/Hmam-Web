8e4dcbeb4a8b88a8d9ad4e4e3d692556f73201f2
HmamSr <humamnuqi2@gmail.com>
2026-02-15 03:45:49 +0700

    fix: add explicit NEXTAUTH_SECRET to authOptions

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,7 @@ import NextAuth, { type NextAuthOptions } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
 export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_ID!,
